8c703f738d5894cb7e953c7c856030bd0c1434c4
Jack Coyle <60261884+Jack-E-Coyle@users.noreply.github.com>
2021-02-12 20:58:47 +0000

    Creating and displaying the movie array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Week-Three-Movie-List';
+  title = 'Movie-List';
+
+  moviesList = [
+    {id : 1, title: 'The Godfather', year: '1972', director: 'Francis Ford Capola'},
+    {id : 2, title: 'Millers Crossing', year: '1990', director: 'The Coen Brothers'},
+    {id : 3, title: 'Dial M for Murder', year: '1954', director: 'Alfred Hitchcock'},
+    {id : 4, title: 'Whiplash', year: '2014', director: 'Damien Chazelle'}
+  ];
 }
