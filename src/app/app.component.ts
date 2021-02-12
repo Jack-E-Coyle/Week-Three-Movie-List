37e65b84bb900beae75da4581804b17e1edd3cc7
Jack Coyle <60261884+Jack-E-Coyle@users.noreply.github.com>
2021-02-12 21:04:26 +0000

    Move movieList array and add getMovies method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Movie-List';
 
-  moviesList = [
-    {id : 1, title: 'The Godfather', year: '1972', director: 'Francis Ford Capola'},
-    {id : 2, title: 'Millers Crossing', year: '1990', director: 'The Coen Brothers'},
-    {id : 3, title: 'Dial M for Murder', year: '1954', director: 'Alfred Hitchcock'},
-    {id : 4, title: 'Whiplash', year: '2014', director: 'Damien Chazelle'}
-  ];
 }
